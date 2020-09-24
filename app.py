4b0662efa061b6ac58aef02decdd3794f35fcf31
Madhav-77 <47825436+Madhav-77@users.noreply.github.com>
2020-09-25 00:15:46 +0530

    give access

--- app.py
+++ app.py
@@ -14,18 +14,11 @@ app.config["ALLOWED_EXTENSIONS"] = ["png", "jpg", "jpeg"]
 mysql = MySQL()
 
 # local MySQL configurations 
-# app.config["UPLOADS_FOLDER"] = "./uploads"
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = ''
-# app.config['MYSQL_DATABASE_DB'] = 'shop_bridge'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
-# prod MySQL configurations 
 app.config["UPLOADS_FOLDER"] = "./uploads"
-app.config['MYSQL_DATABASE_USER'] = 'sql12366947'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'GGX6Kf1rwz'
-app.config['MYSQL_DATABASE_DB'] = 'sql12366947'
-app.config['MYSQL_DATABASE_HOST'] = 'sql12.freemysqlhosting.net'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'shop_bridge'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
 
